6c39c949dff09eddfcae8168dc26184a479f4e7b
dugraphy <91935120+dugraphy@users.noreply.github.com>
2026-06-15 18:38:21 +0900

    Add files via upload

--- 2025_pf/js/main.js
+++ 2025_pf/js/main.js
@@ -93,8 +93,9 @@ $(function () {
     return windowWidth <= 768 ? windowWidth * 0.5 : 300; // 모바일에서는 80% 너비, 기본은 300px
   }
 
-  // 초기 슬라이더 실행
-  initializeSlider();
+  $(window).on('load', function() {
+    initializeSlider();
+  });
 
   // 창 크기 변경 시 슬라이드 크기 재조정
   $(window).resize(function () {
@@ -117,29 +118,26 @@ $(function () {
   const itemsPerRow04 = 3; // 한 줄에 3개씩 로드
   let isLoading04 = false; // 로딩 중인지 확인
 
-  // 현재 스크롤 위치 저장 변수
-  let scrollPosition1 = 0;
+
+  let scrollPosition = 0;
+
+  function lockScroll() {
+    scrollPosition = $(window).scrollTop();
+    $('html, body').css({
+      position: 'fixed',
+      top: `-${scrollPosition}px`,
+      width: '100%'
+    });
+  }
+
+  function unlockScroll() {
+    $('html, body').css({ position: '', top: '', width: '' });
+    $(window).scrollTop(scrollPosition);
+  }
+
 
   // 로드될 이미지 경로 리스트 (poster_1.jpg ~ poster_30.jpg)
   const imagePaths04 = Array.from({ length: 30 }, (_, i) => `./img/poster/poster_${i + 1}.jpg`);
-
-  // 스크롤 잠금 (팝업 열릴 때)
-  function lockScroll() {
-    scrollPosition1 = window.scrollY; // 현재 스크롤 위치 저장
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollPosition1}px`;
-    document.body.style.width = '100%';
-  }
-
-  // 스크롤 잠금 해제 (위치 유지)
-  function unlockScroll() {
-    $('html, body').css({
-      position: '',
-      top: '',
-      width: ''
-    });
-    $(window).scrollTop(scrollPosition1); // 저장된 위치로 복귀
-  }
 
   // 더미 데이터를 생성하는 함수
   function loadMoreItems04() {
@@ -288,28 +286,6 @@ $(function () {
   });
 
   updateSlider();
-
-  var scrollPosition2 = 0; // 현재 스크롤 위치 저장 변수
-
-  // 스크롤 잠금 함수 (위치 유지)
-  function lockScroll() {
-    scrollPosition2 = $(window).scrollTop(); // 현재 스크롤 위치 저장
-    $('html, body').css({
-      position: 'fixed',
-      top: `-${scrollPosition2}px`,
-      width: '100%'
-    });
-  }
-
-  // 스크롤 잠금 해제 (위치 유지)
-  function unlockScroll() {
-    $('html, body').css({
-      position: '',
-      top: '',
-      width: ''
-    });
-    $(window).scrollTop(scrollPosition2); // 저장된 위치로 복귀
-  }
 
   // 팝업 열기
   $('.img-box').click(function () {
